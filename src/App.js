764c9554a820d53f34a2e8fd26c8616b84b9d283
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-09-17 15:50:09 -0700

    added a button to get back to the home page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import PizzaForm from "./components/PizzaForm";
 import "./App.css";
 
 const App = () => {
-
-
-
   return (
     <div class='App'>
       <h1>Lambda Eats</h1>
@@ -14,6 +11,7 @@ const App = () => {
         <Link to='/pizza' style={{style:'inherit'}} id='order-pizza'><button>Order Pizza</button></Link>
       </Route>
       <Route path='/pizza'>
+        <Link to='/' style={{style:'inherit'}} id='back-to-home'><button>Home</button></Link>
         <PizzaForm/>
       </Route>
     </div>
